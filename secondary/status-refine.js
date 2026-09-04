dbc22508d4987879f6562406f06b06eb6346f70c
Eli Black-Mizuta <39786656+elikblack@users.noreply.github.com>
2026-09-04 16:00:07 -0700

    Remove redundant maintenance navigation script

--- secondary/status-refine.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const maintenanceStatus = document.querySelector('.status-maintenance');
-  const maintenanceTab = document.querySelector('.secondary-tab[data-tab="maintenance"]');
-  if (!maintenanceStatus || !maintenanceTab) return;
-
-  maintenanceStatus.addEventListener('click', () => {
-    maintenanceTab.click();
-  });
-})();
